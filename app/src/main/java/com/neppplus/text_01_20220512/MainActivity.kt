2a22c99fcbc71a7a50577e7ed425981d3f604f34
nepp.plus <nepp.plus@gamil.com>
2022-05-12 11:32:23 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/text_01_20220512/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.text_01_20220512
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
